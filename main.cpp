dda32a47f6d6f9a6e7413af53587c67609835752
TREGLIA RAFFAELLA <r.treglia@studenti.uniba.it>
2024-12-06 13:08:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,15 +9,17 @@ int main() {
     dict.inserisci("car", "vehicle");
     dict.inserisci("book", "object");
     
-    //inserisco un caso di collisione.
+    // Inserisco un caso di collisione.
     dict.inserisci("papel", "red");
 
-    
     std::cout << "Value for 'apple': " << dict.recupera("apple") << "\n";
     std::cout << "Value for 'car': " << dict.recupera("car") << "\n";
     std::cout << "Value for 'pen': " << dict.recupera("pen") << "\n";
 
     dict.stampa();
+    std::cout<<" La chiave apple appartiene?"<<dict.appartiene("apple")<<std::endl;
 
+    dict.cancella("car");
+    dict.stampa();
     return 0;
 }
